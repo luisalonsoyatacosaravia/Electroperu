0666ef79ebbceef5e8d37fdb34db3b2db2001db6
Luis Yataco <luisalonsoyatacosaravia@gmail.com>
2025-10-16 12:25:39 -0500

    primer commit

---
+++ controllers/personaController.js
@@ -0,0 +1,63 @@
+const { error } = require('console')
+const db = require('../config/db')
+const path = require('path')
+
+const obtenerTodas = async (req, res) => {
+  try {
+    const sql = "SELECT * FROM personas"
+    const [rows] = await db.query(sql)
+    res.json(rows)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: 'Error en la conexión' })
+  }
+}
+
+// Crear persona
+const crear = async (req, res) => {
+  try {
+    // 1. Recibir los datos del formulario
+    const { apellidos, nombres, dni, telefono } = req.body
+
+    // 2. (Desactivado) Recibir la fotografía real
+    // const fotografia = req.file ? `/uploads/${req.file.filename}` : null;
+
+    const fotografia = 'nueva foto.jpg' // temporal, como dijiste
+
+    // 3. Validaciones básicas
+    if (!apellidos || !nombres || !dni || !telefono) {
+      return res.status(400).json({ error: 'Todos los campos son obligatorios' })
+    }
+
+    // 4. Guardar nuevo registro
+    const [result] = await db.query(
+      "INSERT INTO personas (apellidos, nombres, dni, telefono, fotografia) VALUES (?,?,?,?,?)",
+      [apellidos, nombres, dni, telefono, fotografia]
+    )
+
+    res.status(201).json({
+      id: result.insertId,
+      message: 'Registro correcto'
+    })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: 'Error al registrar persona' })
+  }
+}
+
+// Actualizar persona (opcional)
+const actualizar = async (req, res) => {
+
+}
+
+
+const eliminar = async (req, res) => {
+
+}
+
+module.exports = {
+  obtenerTodas,
+  crear,
+  actualizar,
+  eliminar
+}
